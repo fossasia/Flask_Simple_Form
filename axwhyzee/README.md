1a05815da24d53872bf75d5211d04dc04cabb1d5
LAPTOP-OB9DA8VS\Admin <siah_weehung@yahoo.com>
2017-12-19 13:49:46 +0800

    Simple flask form by axwhyzee

---
+++ axwhyzee/README.md
@@ -0,0 +1,20 @@
+# Flask_Simple_Form
+
+# Deployment
+
+Visit the site @ http://axwhyzee.pythonanywhere.com/
+If site does not work, it means that it has been closed down due to inactivity. Follow the instructions in setup section instead to manually setup and view the web app
+
+# Requirements 
+
+1. python 3.5 -- install python 3.5
+2. flask -- run command prompt and enter "pip install flask"
+
+# Setup
+
+1. From this repository, go to Clone or download and copy the url
+2. Open git bash and enter "git clone <url>"
+3. Navigate to /Flask_Simple_Form/axwhyzee and run app.py
+4. Copy the url from the shell (http://127.0.0.1:5000/) and paste it in any browser
+5. You're set!
+
---
+++ axwhyzee/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, flash
+
+app = Flask(__name__)
+app.secret_key = 'some_secret_key'
+
+
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    print('fkking')
+    if request.method == "POST":
+        flash("Welcome {}! Have a nice day!".format(request.form["username"]))
+        lst = [["Username", request.form["username"]], ["Date of birth", request.form["dob"]],["Email", request.form["email"]], ["Gender", request.form["gender"]]]
+        return render_template("profile.html", lst=lst)
+    else:
+        print('fk')
+        return render_template("homepage.html")
+
+#run app
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
